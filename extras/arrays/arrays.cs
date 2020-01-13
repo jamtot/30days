20342bf9a82e1711716ffdda5b8a022a4abc1744
Jonathan Morris <emailforjmorris@gmail.com>
2020-01-13 10:58:55 +1000

    Added a Python3 and C# solution. They both have very easy built in ways to reverse arrays/lists.

---
+++ extras/arrays/arrays.cs
@@ -0,0 +1,25 @@
+using System.IO;
+using System;
+
+class Solution {
+
+    static int[] reverseArray(int[] a) {
+        Array.Reverse(a);
+        return a;
+    }
+
+    static void Main(string[] args) {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int arrCount = Convert.ToInt32(Console.ReadLine());
+
+        int[] arr = Array.ConvertAll(Console.ReadLine().Split(' '), arrTemp => Convert.ToInt32(arrTemp))
+        ;
+        int[] res = reverseArray(arr);
+
+        textWriter.WriteLine(string.Join(" ", res));
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
