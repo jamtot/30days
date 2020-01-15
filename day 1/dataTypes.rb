0142f7c9dce35cd924b3c0f5065ae1b916bdbb9a
Jonathan Morris <emailforjmorris@gmail.com>
2020-01-15 10:38:32 +1000

    Added a Ruby solution to day 1.

---
+++ day 1/dataTypes.rb
@@ -0,0 +1,16 @@
+i = 4
+d = 4.0
+s = 'HackerRank '
+# Declare second integer, double, and String variables.
+# Read and save an integer, double, and String to your variables.
+i2 = gets
+d2 = gets
+s2 = gets
+
+# Print the sum of both integer variables on a new line.
+puts i+i2.to_i
+# Print the sum of the double variables on a new line.
+puts d+d2.to_f
+# Concatenate and print the String variables on a new line
+# The 's' variable above should be printed first.
+puts s+s2
